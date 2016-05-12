c3a94a96f2907738857d240058d8c6d6246f8deb
Patrick Gross <patrick.gross@native-instruments.de>
2016-05-12 20:30:01 +0200

    Functional test now checks for list item

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,16 +18,31 @@ class NewVisitorTest(unittest.TestCase):
 
 		#Check page title for Django
 		self.assertIn ('To-Do', self.browser.title)
-		self.fail('Finish the test')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)		
+
 		#Create a to do item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
 
 		#Type "buy something" into the text-box
+		inputbox.send_keys('Buy something')
 
 		#Hitting enter, should update the page and page lists
 		#"buy something as to-do item"
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+				any(row.text == '1: Buy something' for row in rows)
+			)
 
 		#Blackbox shows and invites to add another item.
 		#Enter "buy another item"
+		self.fail('Finish the test')
 
 		#Page should update again and both items show on list
 
