da1ef0bee7084fa22f9ddb7f4377bb232fb81e1d
Patrick Gross <patrick.gross@native-instruments.de>
2016-05-12 08:29:20 +0200

    Added unittests and class

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,37 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_create_list_and_retrieve_it_later(self):
+
+		#Open Homepage
+		self.browser.get('http://localhost:8000')
+
+		#Check page title for Django
+		self.assertIn ('To-Do', self.browser.title)
+		self.fail('Finish the test')
+		#Create a to do item
+
+		#Type "buy something" into the text-box
+
+		#Hitting enter, should update the page and page lists
+		#"buy something as to-do item"
+
+		#Blackbox shows and invites to add another item.
+		#Enter "buy another item"
+
+		#Page should update again and both items show on list
+
+		#Check if unique URL was created to store list
+
+		#Visit unique URL and check if items are still there
+
+if __name__ == '__main__':
+	unittest.main()
