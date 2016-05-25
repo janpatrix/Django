57ae2b540711d0c2eb39e7d3df7b6d14ddb38432
Patrick Gross <patrick.gross@native-instruments.de>
2016-05-25 09:44:29 +0200

    Refactor list view

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -48,14 +48,13 @@ class ListViewTest(TestCase):
 		correct_list = List.objects.create()
 		response = self.client.get('lists/%d/' % (correct_list.id,))
 
-class NewListTests(TestCase):
 
 	def test_can_save_a_POST_request_to_an_existing_list(self):
 		other_list = List.objects.create()
 		correct_list = List.objects.create()
 
 		self.client.post(
-			'/lists/%d/add_item' % (correct_list.id),
+			'/lists/%d/' % (correct_list.id),
 			data = {'item_text': 'A new item for an existing list'}
 			)
 
@@ -64,12 +63,12 @@ class NewListTests(TestCase):
 		self.assertEqual(new_item.text, 'A new item for an existing list')
 		self.assertEqual(new_item.list, correct_list)
 
-	def test_redirects_to_list_view(self):
+	def test_POST_redirects_to_list_view(self):
 		other_list = List.objects.create()
 		correct_list = List.objects.create()
 
 		response = self.client.post(
-			'/lists/%d/add_item' % (correct_list.id),
+			'/lists/%d/' % (correct_list.id),
 			data = {'item_text': 'A new item for an existing list'}
 			)
 
